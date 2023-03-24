e7d2fe6908cca76a2312a00c3019b5272b2c2bbd
MarcusAhlfors <marcus.ahlfors@nemesys.fi>
2023-03-24 09:00:03 +0000

    Exclude postgresq from readiness probe

--- status/status.mjs
+++ status/status.mjs
@@ -112,7 +112,7 @@ const getStatus = async () => {
 
   const status = {
     diploiStatusVersion: 1,
-    items: [wwwStatus, await getPostgresStatus()],
+    items: [wwwStatus/*, await getPostgresStatus()*/],
   };
 
   return status;
@@ -127,6 +127,7 @@ const server = http.createServer(requestListener);
 server.listen(3000, '0.0.0.0');
 
 const podReadinessLoop = async () => {
+  
   const status = await getStatus();
   let allOK = !status.items.find((s) => s.status !== Status.GREEN);
   if (allOK) {
