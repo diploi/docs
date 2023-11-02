40a3273de186884431b95efb44639dc848cb71e5
Tuomas Pöyry <me@tuomas.id>
2023-11-02 14:35:34 +0200

    Migrate to Starlight (#6)

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Diploi Docs',
+      logo: {
+        light: './src/assets/logo-text.svg',
+        dark: './src/assets/logo-white-text.svg',
+        replacesTitle: true,
+      },
+      social: {
+        github: 'https://github.com/diploi/docs',
+      },
+      sidebar: [
+        {
+          label: 'Introduction',
+          items: [{ label: 'Get Started', link: '/get-started/' }],
+        },
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        },
+        {
+          label: 'Concepts',
+          autogenerate: { directory: 'concepts' },
+        },
+        {
+          label: 'Templates',
+          autogenerate: { directory: 'templates' },
+        },
+      ],
+      customCss: ['./src/styles/custom.css'],
+    }),
+  ],
+});
